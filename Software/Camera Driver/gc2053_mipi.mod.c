2af8832d247181daf237f6833ee8f24fd51def87
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-12-20 12:17:20 +0800

    add compile readme

---
+++ Software/Camera Driver/gc2053_mipi.mod.c
@@ -0,0 +1,29 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+MODULE_INFO(intree, "Y");
+
+#ifdef RETPOLINE
+MODULE_INFO(retpoline, "Y");
+#endif
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=vin_io";
+
+MODULE_ALIAS("i2c:gc2053_mipi_2");
+MODULE_ALIAS("i2c:gc2053_mipi");
