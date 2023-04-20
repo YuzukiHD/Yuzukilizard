2a59057e2548026693f67b9f46f0c710755343e4
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-04-20 13:04:20 +0800

    [sunxi-mpp] add sample_demux2adec2ao demo

---
+++ Software/sunxi-mpp/sample/sample_demux2adec2ao/sample_demux2adec2ao_common.h
@@ -0,0 +1,15 @@
+#ifndef __DEMUX2ADEC2AO_DEMO_COMMON_H__
+#define __DEMUX2ADEC2AO_DEMO_COMMON_H__
+
+
+#define DMX2ADEC2AO_CFG_SRC_FILE_STR    "src_file"
+#define DMX2ADEC2AO_CFG_SRC_SIZE        "src_size"
+//#define DMX2VDEC_CFG_DST_Y_FILE_STR   "y_dst_file"
+//#define DMX2VDEC_CFG_DST_U_FILE_STR   "u_dst_file"
+//#define DMX2VDEC_CFG_DST_V_FILE_STR   "v_dst_file"
+//#define DMX2VDEC_CFG_DST_YUV_FILE_STR "yuv_dst_file"
+#define DMX2ADEC2AO_CFG_SEEK_POSITION   "seek_position"
+
+
+#endif //#define __DEMUX2ADEC2AO_DEMO_COMMON_H__
+
