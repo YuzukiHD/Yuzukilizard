dd35e08f1aedd6e898629078892f35d4e6d97c40
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-04-20 13:04:20 +0800

    [sunxi-mpp] add sample_demux2adec demo

---
+++ Software/sunxi-mpp/sample/sample_demux2adec/sample_demux2adec_conf.h
@@ -0,0 +1,9 @@
+#ifndef __DEMUX2ADEC_CONFIG_H__
+#define __DEMUX2ADEC_CONFIG_H__
+
+#define DMX2ADEC_CFG_SRC_FILE_PATH	"src_file"
+#define DMX2ADEC_CFG_DST_FILE_PATH	"dst_file"
+#define DMX2ADEC_CFG_SEEK_POSITION	"seek_position"
+
+#endif //#define __DEMUX2ADEC_CONFIG_H__
+
